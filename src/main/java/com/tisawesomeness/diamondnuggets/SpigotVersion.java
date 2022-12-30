f0ee225f0f65fdcf90b93251134b997d0ef9957b
Tisawesomeness <tis_dev@protonmail.com>
2022-12-30 00:11:54 -0500

    Fix valid spigot versions showing as unsupported

--- src/main/java/com/tisawesomeness/diamondnuggets/SpigotVersion.java
+++ src/main/java/com/tisawesomeness/diamondnuggets/SpigotVersion.java
@@ -2,17 +2,19 @@ package com.tisawesomeness.diamondnuggets;
 
 import org.bukkit.Bukkit;
 
+import java.util.Comparator;
+
 public enum SpigotVersion {
-    V1_16_5("1.16.5", 6),
-    V1_17_1("1.17.1", 7),
-    V1_18_2("1.18.2", 8),
+    V1_16_2("1.16.2", 6),
+    V1_17("1.17", 7),
+    V1_18("1.18", 8),
     V1_19("1.19", 9),
     V1_19_3("1.19.3", 12);
 
-    private final String version;
+    private final Version version;
     public final int packFormat;
     SpigotVersion(String version, int packFormat) {
-        this.version = version;
+        this.version = Version.parse(version);
         this.packFormat = packFormat;
     }
 
@@ -20,16 +22,18 @@ public enum SpigotVersion {
 
     private static SpigotVersion initVersion() {
         String bukkitVersion = Bukkit.getServer().getBukkitVersion();
-        SpigotVersion version = from(bukkitVersion);
+        SpigotVersion version = findLatestPackVersion(bukkitVersion);
         if (version == null) {
             throw new IllegalStateException("Unsupported Spigot version: " + bukkitVersion);
         }
         return version;
     }
-    public static SpigotVersion from(String version) {
+    public static SpigotVersion findLatestPackVersion(String version) {
         String shortVersion = version.substring(0, version.indexOf('-'));
-        for (SpigotVersion v : values()) {
-            if (shortVersion.equalsIgnoreCase(v.version)) {
+        Version currentVersion = Version.parse(shortVersion);
+        for (int i = values().length - 1; i >= 0; i--) {
+            SpigotVersion v = values()[i];
+            if (currentVersion.compareTo(v.version) >= 0) {
                 return v;
             }
         }
@@ -38,6 +42,28 @@ public enum SpigotVersion {
 
     public boolean supportsRecipeBookCategory() {
         return this.compareTo(V1_19_3) >= 0;
+    }
+
+    private record Version(int major, int minor, int patch) implements Comparable<Version> {
+
+        private static final Comparator<Version> COMPARATOR = Comparator
+                .comparingInt((Version v) -> v.major)
+                .thenComparingInt(v -> v.minor)
+                .thenComparingInt(v -> v.patch);
+
+        public static Version parse(String str) {
+            String[] split = str.split("\\.");
+            int major = Integer.parseInt(split[0]);
+            int minor = Integer.parseInt(split[1]);
+            int patch = split.length > 2 ? Integer.parseInt(split[2]) : 0;
+            return new Version(major, minor, patch);
+        }
+
+        @Override
+        public int compareTo(Version version) {
+            return COMPARATOR.compare(this, version);
+        }
+
     }
 
 }
