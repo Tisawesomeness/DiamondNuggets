10d1cfa6a36f001a977c64ff82b1c79c32e995bd
Tisawesomeness <tis_dev@protonmail.com>
2024-04-23 21:25:51 -0400

    Update pack versions

--- src/main/java/com/tisawesomeness/diamondnuggets/SpigotVersion.java
+++ src/main/java/com/tisawesomeness/diamondnuggets/SpigotVersion.java
@@ -9,7 +9,12 @@ public enum SpigotVersion {
     V1_17("1.17", 7),
     V1_18("1.18", 8),
     V1_19("1.19", 9),
-    V1_19_3("1.19.3", 12);
+    V1_19_3("1.19.3", 12),
+    V1_19_4("1.19.4", 13),
+    V1_20("1.20", 15),
+    V_1_20_2( "1.20.2", 18),
+    V_1_20_3( "1.20.3", 22),
+    V_1_20_5( "1.20.5", 32);
 
     private final Version version;
     public final int packFormat;
@@ -29,8 +34,15 @@ public enum SpigotVersion {
         return version;
     }
     public static SpigotVersion findLatestPackVersion(String version) {
-        String shortVersion = version.substring(0, version.indexOf('-'));
+        int idx = version.indexOf('-');
+        if (idx == -1) {
+            return null;
+        }
+        String shortVersion = version.substring(0, idx);
         Version currentVersion = Version.parse(shortVersion);
+        if (currentVersion == null) {
+            return null;
+        }
         for (int i = values().length - 1; i >= 0; i--) {
             SpigotVersion v = values()[i];
             if (currentVersion.compareTo(v.version) >= 0) {
@@ -53,6 +65,9 @@ public enum SpigotVersion {
 
         public static Version parse(String str) {
             String[] split = str.split("\\.");
+            if (split.length < 2) {
+                return null;
+            }
             int major = Integer.parseInt(split[0]);
             int minor = Integer.parseInt(split[1]);
             int patch = split.length > 2 ? Integer.parseInt(split[2]) : 0;
